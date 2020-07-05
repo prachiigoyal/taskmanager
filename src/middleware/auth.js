0ee4fb74890b1e9c6351ef1593eea3f27271bb11
Mohit Sikri <sikrimohit454.com>
2020-07-06 04:06:03 +0530

    deleting

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const User=require('../models/user')
 const auth= async(req,res,next)=>{
     try{
         const token = req.header('Authorization').replace('Bearer ','')
-   const decoded = jwt.decode(token,process.env.JWT_TOKEN)
+   const decoded = jwt.decode(token , process.env.JWT_TOKEN)
    const user = await User.findOne({_id:decoded._id,'tokens.token': token})
    if(!user){
        throw new Error()
